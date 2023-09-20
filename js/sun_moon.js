d31e1807ad3db34d629c35c88e49443713b81e32
linshiol <1525506337@qq.com>
2023-09-20 17:18:41 +0800

    Site updated: 2023-09-20 17:18:41

--- js/sun_moon.js
+++ js/sun_moon.js
@@ -72,4 +72,19 @@ function switchNightMode() {
     typeof utterancesTheme === 'function' && utterancesTheme()
     typeof FB === 'object' && window.loadFBComment()
     window.DISQUS && document.getElementById('disqus_thread').children.length && setTimeout(() => window.disqusReset(), 200)
+    
+    
+    
+    // 发现有时会和当前页面重复，加一个判断
+function randomPost() {
+    fetch('/baidusitemap.xml').then(res => res.text()).then(str => (new window.DOMParser()).parseFromString(str, "text/xml")).then(data => {
+        let ls = data.querySelectorAll('url loc');
+        while (true) {
+            let url = ls[Math.floor(Math.random() * ls.length)].innerHTML;
+            if (location.href == url) continue;
+            location.href = url;
+            return;
+        }
+    })
+}
 }
